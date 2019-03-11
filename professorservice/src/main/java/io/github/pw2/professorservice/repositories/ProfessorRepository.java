ad81e931f423900f43768eb873c0c693134972b7
Lucas Duete <lm.duete@gmail.com>
2019-03-11 18:18:06 -0300

    ProfessorService: Implementado metodo de buscar por nome em ProfessorService, implementados metodos de listagem, busca por matricula e nome em ProfessorService

--- professorservice/src/main/java/io/github/pw2/professorservice/repositories/ProfessorRepository.java
+++ professorservice/src/main/java/io/github/pw2/professorservice/repositories/ProfessorRepository.java
@@ -4,7 +4,11 @@ import io.github.pw2.professorservice.models.Professor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ProfessorRepository extends JpaRepository<Professor, Long> {
+
+    public Optional<Professor> findByNome(String nome);
 
 }
--- professorservice/src/main/java/io/github/pw2/professorservice/services/ProfessorService.java
+++ professorservice/src/main/java/io/github/pw2/professorservice/services/ProfessorService.java
@@ -1,8 +1,12 @@
 package io.github.pw2.professorservice.services;
 
+import com.google.common.collect.ImmutableList;
 import io.github.pw2.professorservice.models.Professor;
 import io.github.pw2.professorservice.repositories.ProfessorRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProfessorService {
@@ -15,6 +19,18 @@ public class ProfessorService {
 
     public Professor salvar(Professor professor) {
         return this.repository.save(professor);
+    }
+
+    public List<Professor> listarAll() {
+        return ImmutableList.copyOf(this.repository.findAll());
+    }
+
+    public Optional<Professor> buscarPorMatricula(Long matricula) {
+        return this.repository.findById(matricula);
+    }
+
+    public Optional<Professor> buscarPorNome(String nome) {
+        return this.repository.findByNome(nome);
     }
 
 }
