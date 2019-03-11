03788cfa82cc7992ee62f461ee9d2764fbbf938e
Lucas Duete <lm.duete@gmail.com>
2019-03-11 13:39:29 -0300

    Adicionado Projeto de HorarioService

---
+++ horarioservice/src/main/java/io/github/pw2/horarioservice/HorarioserviceApplication.java
@@ -0,0 +1,13 @@
+package io.github.pw2.horarioservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HorarioserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HorarioserviceApplication.class, args);
+    }
+
+}
---
+++ horarioservice/src/test/java/io/github/pw2/horarioservice/HorarioserviceApplicationTests.java
@@ -0,0 +1,16 @@
+package io.github.pw2.horarioservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HorarioserviceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
