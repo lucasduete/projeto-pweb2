0b828c95f146a549ad6f5684f749f0e2425393ba
Lucas Duete <lm.duete@gmail.com>
2019-03-11 13:34:52 -0300

    Adicionado Projeto de CursoService

---
+++ cursoservice/src/main/java/io/github/pw2/cursoservice/CursoserviceApplication.java
@@ -0,0 +1,13 @@
+package io.github.pw2.cursoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CursoserviceApplication.class, args);
+    }
+
+}
