2fd09cc78340cdf7b327e4d3415a657ff6dd2228
Lucas Duete <lm.duete@gmail.com>
2019-03-11 13:36:48 -0300

    Adicionado Projeto de CoordenadorService

---
+++ coordenadorservice/src/main/java/io/github/pw2/coordenadorservice/CoordenadorserviceApplication.java
@@ -0,0 +1,13 @@
+package io.github.pw2.coordenadorservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoordenadorserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoordenadorserviceApplication.class, args);
+    }
+
+}
