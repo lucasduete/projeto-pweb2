103bce17d37b9eb1f371713933f55b1d82d0548e
Lucas Duete <lm.duete@gmail.com>
2019-03-11 17:40:02 -0300

    AmbienteService: Implementado metodo de busca de ambiente por nome em AmbienteRepository, Implementados metodos de busca por todos, busca por codigo e busca por nome em AmbienteService

--- ambienteservice/src/main/java/io/github/pw2/ambienteservice/repositories/AmbienteRepository.java
+++ ambienteservice/src/main/java/io/github/pw2/ambienteservice/repositories/AmbienteRepository.java
@@ -4,7 +4,11 @@ import io.github.pw2.ambienteservice.models.Ambiente;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface AmbienteRepository extends JpaRepository<Ambiente, String> {
+
+    public Optional<Ambiente> findByNome(String nome);
 
 }
--- ambienteservice/src/main/java/io/github/pw2/ambienteservice/services/AmbienteService.java
+++ ambienteservice/src/main/java/io/github/pw2/ambienteservice/services/AmbienteService.java
@@ -1,8 +1,12 @@
 package io.github.pw2.ambienteservice.services;
 
+import com.google.common.collect.ImmutableList;
 import io.github.pw2.ambienteservice.models.Ambiente;
 import io.github.pw2.ambienteservice.repositories.AmbienteRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class AmbienteService {
@@ -15,6 +19,18 @@ public class AmbienteService {
 
     public Ambiente salvar(Ambiente ambiente) {
         return this.repository.save(ambiente);
+    }
+
+    public List<Ambiente> listarTodos() {
+        return ImmutableList.copyOf(this.repository.findAll());
+    }
+
+    public Optional<Ambiente> recuperarPorCodigo(String codigo) {
+        return this.repository.findById(codigo);
+    }
+
+    public Optional<Ambiente> recuperarPorNome(String nome) {
+        return this.repository.findByNome(nome);
     }
 
 }
