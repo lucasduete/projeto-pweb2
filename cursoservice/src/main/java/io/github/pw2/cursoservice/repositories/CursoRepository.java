e8934d00d5f420f82021c2aa52412abca8c49b3b
Lucas Duete <lm.duete@gmail.com>
2019-03-11 18:31:47 -0300

    CursoService: Implementado metodo de busca de Curso por nome em CursoRepository, Implementados metodos de listagem, busca por codigo e por nome em CursoRepository.

--- cursoservice/src/main/java/io/github/pw2/cursoservice/repositories/CursoRepository.java
+++ cursoservice/src/main/java/io/github/pw2/cursoservice/repositories/CursoRepository.java
@@ -1,10 +1,16 @@
 package io.github.pw2.cursoservice.repositories;
 
+import com.google.common.collect.ImmutableList;
 import io.github.pw2.cursoservice.models.Curso;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface CursoRepository extends JpaRepository<Curso, Long> {
+
+    public Optional<Curso> findByNome(String nome);
 
 }
--- cursoservice/src/main/java/io/github/pw2/cursoservice/services/CursoService.java
+++ cursoservice/src/main/java/io/github/pw2/cursoservice/services/CursoService.java
@@ -1,8 +1,12 @@
 package io.github.pw2.cursoservice.services;
 
+import com.google.common.collect.ImmutableList;
 import io.github.pw2.cursoservice.models.Curso;
 import io.github.pw2.cursoservice.repositories.CursoRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CursoService {
@@ -15,6 +19,18 @@ public class CursoService {
 
     public Curso salvar(Curso curso) {
         return this.repository.save(curso);
+    }
+
+    public List<Curso> listarAll() {
+        return ImmutableList.copyOf(this.repository.findAll());
+    }
+
+    public Optional<Curso> buscarPorCodigo(Long codigo) {
+        return this.repository.findById(codigo);
+    }
+
+    public Optional<Curso> buscarPorNome(String nome) {
+        return this.repository.findByNome(nome);
     }
 
 }
