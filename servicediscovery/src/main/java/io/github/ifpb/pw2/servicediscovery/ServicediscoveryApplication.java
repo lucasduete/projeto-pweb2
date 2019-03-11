290267ff2c84623b211d2ebcfcf9d8d1f70cbef2
Lucas Duete <lm.duete@gmail.com>
2019-03-11 13:29:15 -0300

    Adicionado Projeto de  Service Discovery

---
+++ servicediscovery/src/main/java/io/github/ifpb/pw2/servicediscovery/ServicediscoveryApplication.java
@@ -0,0 +1,13 @@
+package io.github.ifpb.pw2.servicediscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicediscoveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServicediscoveryApplication.class, args);
+    }
+
+}
