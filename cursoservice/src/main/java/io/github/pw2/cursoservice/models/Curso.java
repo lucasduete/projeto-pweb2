97c4c43a0eb6ae0272b2b60e47c5ab93499ba07b
Lucas Duete <lm.duete@gmail.com>
2019-03-11 15:21:32 -0300

    CursoService: Implementadas classes de modelagem para Curso e Disciplina

---
+++ cursoservice/src/main/java/io/github/pw2/cursoservice/models/Curso.java
@@ -0,0 +1,29 @@
+package io.github.pw2.cursoservice.models;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
+public class Curso {
+
+    @Id
+    private Long codigo;
+
+    @Column(nullable = false, unique = true)
+    private String nome;
+
+    @Column(nullable = false)
+    private String descricao;
+
+    @OneToMany(fetch = FetchType.LAZY, orphanRemoval = true)
+    private List<Disciplina> disciplinas;
+
+}
---
+++ cursoservice/src/main/java/io/github/pw2/cursoservice/models/Disciplina.java
@@ -0,0 +1,22 @@
+package io.github.pw2.cursoservice.models;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
+public class Disciplina {
+
+    @Id
+    private Long codigo;
+
+    @Column(nullable = false)
+    private String nome;
+
+}
