39b261f25bdf2a123d7fa897ab9e7c8b2056e7ba
Lucas Duete <lm.duete@gmail.com>
2019-03-11 14:10:09 -0300

    Ativado ServiceDiscovery e corrigida a porta de execuçao para CursoService

--- cursoservice/src/main/java/io/github/pw2/cursoservice/CursoserviceApplication.java
+++ cursoservice/src/main/java/io/github/pw2/cursoservice/CursoserviceApplication.java
@@ -2,8 +2,10 @@ package io.github.pw2.cursoservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CursoserviceApplication {
 
     public static void main(String[] args) {
