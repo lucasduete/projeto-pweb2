b35fc30fde0fa343c4217123cc85a11a80eaa75a
Lucas Duete <lm.duete@gmail.com>
2019-03-11 13:31:55 -0300

    Adicionado Projeto de AmbienteService

---
+++ ambienteservice/src/main/java/io/github/pw2/ambienteservice/AmbienteserviceApplication.java
@@ -0,0 +1,13 @@
+package io.github.pw2.ambienteservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmbienteserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmbienteserviceApplication.class, args);
+    }
+
+}
---
+++ ambienteservice/src/test/java/io/github/pw2/ambienteservice/AmbienteserviceApplicationTests.java
@@ -0,0 +1,16 @@
+package io.github.pw2.ambienteservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AmbienteserviceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
