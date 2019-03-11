016d03004a6e5cac7e29fd34b4b5a72f15fa6002
Lucas Duete <lm.duete@gmail.com>
2019-03-11 13:38:04 -0300

    Adicionado Projeto de ProfessorService

---
+++ professorservice/src/main/java/io/github/pw2/professorservice/ProfessorserviceApplication.java
@@ -0,0 +1,13 @@
+package io.github.pw2.professorservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfessorserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProfessorserviceApplication.class, args);
+    }
+
+}
