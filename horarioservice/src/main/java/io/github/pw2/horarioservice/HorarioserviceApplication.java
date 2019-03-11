4bfa58b396b026b08c9329cf9a5edbae138a5d99
Lucas Duete <lm.duete@gmail.com>
2019-03-11 14:07:07 -0300

    Adicionadas as configuracoes de HorarioService

--- horarioservice/src/main/java/io/github/pw2/horarioservice/HorarioserviceApplication.java
+++ horarioservice/src/main/java/io/github/pw2/horarioservice/HorarioserviceApplication.java
@@ -2,8 +2,10 @@ package io.github.pw2.horarioservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class HorarioserviceApplication {
 
     public static void main(String[] args) {
