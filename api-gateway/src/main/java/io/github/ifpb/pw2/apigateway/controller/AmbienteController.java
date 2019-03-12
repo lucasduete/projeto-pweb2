e0ee255950c836e91ce5a9d25146dc6cfb6bbbfc
Lucas Duete <lm.duete@gmail.com>
2019-03-11 21:20:23 -0300

    Api-Gateway: Implementados metodos de listagem em AmbienteController

--- api-gateway/src/main/java/io/github/ifpb/pw2/apigateway/controller/AmbienteController.java
+++ api-gateway/src/main/java/io/github/ifpb/pw2/apigateway/controller/AmbienteController.java
@@ -2,12 +2,8 @@ package io.github.ifpb.pw2.apigateway.controller;
 
 import io.github.ifpb.pw2.apigateway.feingClients.AmbienteClient;
 import io.github.pw2.ambienteservice.models.Ambiente;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("ambiente")
@@ -22,6 +18,27 @@ public class AmbienteController {
     @PostMapping
     public ResponseEntity salvarAmbiente(@RequestBody Ambiente ambiente) {
         ResponseEntity response = this.ambienteClient.salvarAmbiente(ambiente);
+
+        return ResponseEntity.status(response.getStatusCode()).body(response.getBody());
+    }
+
+    @GetMapping
+    public ResponseEntity listarTodos() {
+        ResponseEntity response = this.ambienteClient.listarTodos();
+
+        return ResponseEntity.status(response.getStatusCode()).body(response.getBody());
+    }
+
+    @GetMapping("/{codigo}")
+    public ResponseEntity buscarPorCodigo(@PathVariable("codigo") String codigo) {
+        ResponseEntity response = this.ambienteClient.buscarPorCodigo(codigo);
+
+        return ResponseEntity.status(response.getStatusCode()).body(response.getBody());
+    }
+
+    @GetMapping("/buscar")
+    public ResponseEntity buscarPorNome(@RequestParam(name = "nome", required = true) String nome) {
+        ResponseEntity response = this.ambienteClient.buscarPorNome(nome);
 
         return ResponseEntity.status(response.getStatusCode()).body(response.getBody());
     }
