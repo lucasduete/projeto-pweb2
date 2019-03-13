1d0a6bc95578f39291816b6d79eee800130b5d56
Lucas Duete <lm.duete@gmail.com>
2019-03-13 12:55:03 -0300

    ServiceDiscovery: Configurando ServiceDiscovery para funcionar como Spring Cloud Config

--- servicediscovery/src/main/java/io/github/ifpb/pw2/servicediscovery/ServicediscoveryApplication.java
+++ servicediscovery/src/main/java/io/github/ifpb/pw2/servicediscovery/ServicediscoveryApplication.java
@@ -2,10 +2,12 @@ package io.github.ifpb.pw2.servicediscovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
+@EnableConfigServer
 public class ServicediscoveryApplication {
 
     public static void main(String[] args) {
