3ceb4b49e7e0a5d2685faf7d51fe9da010545e1c
FlavioHenriique <flaviohenrique638@gmail.com>
2019-03-15 22:36:26 -0300

    Corrigindo listagem de ambientes (observable e subscribe)

--- front-end-pweb2/src/app/ambiente.service.ts
+++ front-end-pweb2/src/app/ambiente.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Ambiente } from './ambiente';
@@ -12,14 +12,14 @@ const httpOptions = {
 })
 export class AmbienteService {
   
-  private url = 'localhost:8761/ambiente'
+  private url = 'http://localhost:8080/ambiente';
   
   constructor(
     private http : HttpClient
   ) { }
   
-  getAmbientes() : Observable<Ambiente[]>{
-    return this.http.get<Ambiente[]>(this.url);
+  getAmbientes() : Observable<HttpResponse<Ambiente[]>>{
+    return this.http.get<Ambiente[]>(this.url, {observe:'response'});
   }
 
   addAmbinete(ambiente : Ambiente) : Observable<any>{
