aa16d699d626a3639ba148d9d69462756a470e3c
Lucas Duete <lm.duete@gmail.com>
2019-03-25 18:45:58 -0300

    Api-Gateway: Adicionada diretiva consumes para especificar Content-Type para metodo REST de login

--- api-gateway/src/main/java/io/github/ifpb/pw2/apigateway/controller/LoginController.java
+++ api-gateway/src/main/java/io/github/ifpb/pw2/apigateway/controller/LoginController.java
@@ -5,13 +5,17 @@ import io.github.ifpb.pw2.apigateway.service.jwt.TokenProvider;
 import io.github.pw2.coordenadorservice.models.Coordenador;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("login")
@@ -29,7 +33,7 @@ public class LoginController {
         this.authenticationManager = authenticationManager;
     }
 
-    @PostMapping
+    @PostMapping(consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
     public ResponseEntity<Object> login(@RequestParam(name = "matricula", required = true) final String matricula,
                                         @RequestParam(name = "senha", required = true) final String senha) {
 
