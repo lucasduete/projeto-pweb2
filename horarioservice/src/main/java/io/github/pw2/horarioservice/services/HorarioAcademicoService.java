6ba3538ddf0a0232b2e088afe29bf6d0e2b75ff1
Lucas Duete <lm.duete@gmail.com>
2019-03-24 23:50:37 -0300

    HorarioService: Implementado metodo de listagem de HorarioAcademico por curso em HorarioAcademico Service e Repository

--- horarioservice/src/main/java/io/github/pw2/horarioservice/services/HorarioAcademicoService.java
+++ horarioservice/src/main/java/io/github/pw2/horarioservice/services/HorarioAcademicoService.java
@@ -1,10 +1,15 @@
 package io.github.pw2.horarioservice.services;
 
+import com.google.common.collect.ImmutableList;
+import io.github.pw2.horarioservice.exceptions.CursoSemHorarioAcademicoException;
 import io.github.pw2.horarioservice.models.HorarioAcademico;
 import io.github.pw2.horarioservice.repositories.HorarioAcademicoRepository;
 import org.springframework.stereotype.Service;
 
 import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class HorarioAcademicoService {
@@ -16,9 +21,28 @@ public class HorarioAcademicoService {
         this.repository = repository;
     }
 
-    public HorarioAcademico salvar(@NotNull final HorarioAcademico horarioAcademico) {
+    public HorarioAcademico salvarHorario(@NotNull final HorarioAcademico horarioAcademico) {
 
         return this.repository.save(horarioAcademico);
+    }
+
+    public List<HorarioAcademico> listarPorCurso(@NotNull final Long codigoCurso) throws CursoSemHorarioAcademicoException {
+
+        Optional<List<HorarioAcademico>> optionalList = this.repository.findAllByCodigoCurso(codigoCurso);
+
+        if (!optionalList.isPresent()) throw new CursoSemHorarioAcademicoException();
+
+        return ImmutableList.copyOf(optionalList.get());
+    }
+
+    public List<HorarioAcademico> listarPorProfessor(String matriculaProfessor) {
+        // todo
+        return new ArrayList<>();
+    }
+
+    public List<HorarioAcademico> listarPorAmbiente() {
+        // todo
+        return new ArrayList<>();
     }
 
 }
