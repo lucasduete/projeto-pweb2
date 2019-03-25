0ecb8fbd3fa327b30e76d3cdf3b0f147c1b53ab0
Lucas Duete <lm.duete@gmail.com>
2019-03-25 17:29:36 -0300

    Api-Gateway: Reativado Controller REST de Coordeandor em Api-Gateway para serviço de login

--- api-gateway/src/main/java/io/github/ifpb/pw2/apigateway/controller/CoordenadorController.java
+++ api-gateway/src/main/java/io/github/ifpb/pw2/apigateway/controller/CoordenadorController.java
@@ -1,16 +1,15 @@
 package io.github.ifpb.pw2.apigateway.controller;
 
-import ch.qos.logback.core.pattern.util.RegularEscapeUtil;
 import io.github.ifpb.pw2.apigateway.feingClients.CoordenadorClient;
 import io.github.pw2.coordenadorservice.models.Coordenador;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
-//@RestController
-//@RequestMapping("coordenador")
+@RestController
+@RequestMapping("coordenador")
 public class CoordenadorController {
 
     private CoordenadorClient client;
@@ -20,26 +19,12 @@ public class CoordenadorController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<Coordenador> login(@RequestBody Coordenador coordenador){
+    public ResponseEntity<Coordenador> login(@RequestBody Coordenador coordenador) {
         Coordenador buscado = client.recuperar(coordenador.getMatricula()).getBody();
-        if(buscado!= null && buscado.getSenha().equals(coordenador.getSenha())){
+        if (buscado != null && buscado.getSenha().equals(coordenador.getSenha())) {
             return ResponseEntity.ok(buscado);
         }
         return ResponseEntity.notFound().build();
     }
 
-    @PostMapping
-    public ResponseEntity<Coordenador> salvar(@RequestBody Coordenador coordenador){
-        return this.client.saveCoordenador(coordenador);
-    }
-
-    @GetMapping
-    public ResponseEntity<List<Coordenador>> listarTodos(){
-        return this.client.recuperarTodos();
-    }
-
-    @GetMapping("/{matricula}")
-    public ResponseEntity<Coordenador> recuperar(@PathVariable("matricula") String matricula){
-        return this.client.recuperar(matricula);
-    }
 }
