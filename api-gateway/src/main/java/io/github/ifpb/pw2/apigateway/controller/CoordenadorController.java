b477f59baac4027fe71cf87b8c7e4edd2968c33a
Lucas Duete <lm.duete@gmail.com>
2019-03-25 17:58:58 -0300

    Api-Gateway: Removido metodo de login de CoordenadorController.

--- api-gateway/src/main/java/io/github/ifpb/pw2/apigateway/controller/CoordenadorController.java
+++ api-gateway/src/main/java/io/github/ifpb/pw2/apigateway/controller/CoordenadorController.java
@@ -1,44 +1,36 @@
 package io.github.ifpb.pw2.apigateway.controller;
 
+import ch.qos.logback.core.pattern.util.RegularEscapeUtil;
 import io.github.ifpb.pw2.apigateway.feingClients.CoordenadorClient;
 import io.github.pw2.coordenadorservice.models.Coordenador;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
-@RequestMapping("coordenador")
+import java.util.List;
+
+//@RestController
+//@RequestMapping("coordenador")
 public class CoordenadorController {
 
-    private final CoordenadorClient client;
-    private final PasswordEncoder passwordEncoder;
+    private CoordenadorClient client;
 
-    public CoordenadorController(CoordenadorClient client, PasswordEncoder passwordEncoder) {
+    public CoordenadorController(CoordenadorClient client) {
         this.client = client;
-        this.passwordEncoder = passwordEncoder;
     }
 
-    @PostMapping("/login")
-    public ResponseEntity<Object> login(@RequestParam(name = "matricula", required = true) final String matricula,
-                                        @RequestParam(name = "senha", required = true) final String senha) {
-
-        if (matricula == null || matricula.isEmpty()) {
-            return ResponseEntity.badRequest().body("Para efeturar o login voce deve enviar a matricula do Coordenador");
-        } else if (senha == null || senha.isEmpty()) {
-            return ResponseEntity.badRequest().body("Para efeturar o login voce deve enviar a senha do Coordenador");
-        }
-
-        Coordenador coordenadorDB = client.recuperar(matricula).getBody();
-
-        if (coordenadorDB == null) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-        } else if (passwordEncoder.matches(senha, coordenadorDB.getSenha())) {
-            return ResponseEntity.ok(coordenadorDB);
-        } else {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-        }
-
+    @PostMapping
+    public ResponseEntity<Coordenador> salvar(@RequestBody Coordenador coordenador){
+        return this.client.saveCoordenador(coordenador);
     }
 
+    @GetMapping
+    public ResponseEntity<List<Coordenador>> listarTodos(){
+        return this.client.recuperarTodos();
+    }
+
+    @GetMapping("/{matricula}")
+    public ResponseEntity<Coordenador> recuperar(@PathVariable("matricula") String matricula){
+        return this.client.recuperar(matricula);
+    }
 }
