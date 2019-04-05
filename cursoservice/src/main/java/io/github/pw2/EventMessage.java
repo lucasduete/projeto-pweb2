4a64afc9455649fadf5fc004b3f9510e377a0604
Lucas Duete <lm.duete@gmail.com>
2019-04-05 02:20:48 -0300

    CursoService: Configurado proojeto para disparar eventos de persistencia, atualização e remoção de Curso

---
+++ cursoservice/src/main/java/io/github/pw2/EventMessage.java
@@ -0,0 +1,30 @@
+package io.github.pw2;
+
+import lombok.*;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+@Builder
+@ToString
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+public final class EventMessage implements Serializable {
+
+    private ServiceType serviceName;
+    private Object payload;
+    private Operation operation;
+
+    public enum ServiceType {
+
+        HORARIOSERVICE, PROFESSORSERVICE, AMBIENTESERVICE, CURSOSERVICE
+    }
+
+    public enum Operation {
+
+        PERSIST, UPDATE, DELETE
+    }
+
+}
